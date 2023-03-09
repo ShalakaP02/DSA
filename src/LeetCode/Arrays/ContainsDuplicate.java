9ac3e0dcb44c6f2363102a7cd0b285fd3c800bd7
ShalakaP02 <shala.p02@gmail.com>
2023-03-09 15:21:27 +0530

    practice dsa

---
+++ src/LeetCode/Arrays/ContainsDuplicate.java
@@ -0,0 +1,29 @@
+package LeetCode.Arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1,2,3,4}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hset = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+
+            if (!hset.add(nums[i])) {
+                return true;
+            }
+            //hset.add(nums[i]);
+
+        }
+        return false;
+
+//        if(nums.length != hset.size()){
+//            return true;
+//        }else{
+//            return false;
+//        }
+    }
+}
+
