586a351f07a45e36d752aaa38bcae3b095f59bd1
ShalakaP02 <shala.p02@gmail.com>
2023-03-16 18:28:17 +0530

    dsa practice

--- src/LeetCode/Arrays/BestTimeToBuySellStock.java
+++ src/LeetCode/Arrays/BestTimeToBuySellStock.java
@@ -3,35 +3,25 @@ package LeetCode.Arrays;
 public class BestTimeToBuySellStock {
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{2,4,1}));
+        System.out.println(maxProfit(new int[]{4,2,5,1}));
     }
 
     public static int maxProfit(int[] prices) {
-        int min = prices[0];
-        int pos = 0;
-        for(int i = 1; i < prices.length ;i++) {
 
-            if(min > prices[i]){
-                min = prices[i];
-                pos = i;
+        int lsf = Integer.MAX_VALUE;
+        int op = 0;
+        int diff = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
             }
-
-        }
-
-        int max = 0;
-        for(int i = pos+1 ; i < prices.length ;i++) {
-            if(max < prices[i]){
-                max = prices[i];
+            diff = prices[i] - lsf;
+            if(op < diff){
+                op = diff;
             }
         }
-        if (max == 0) {
-            return 0;
-        }else {
-
-            return max - min;
-
-        }
-
+        return op;
 
     }
 }
