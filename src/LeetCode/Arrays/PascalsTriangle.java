caa590e7e84a30b16494d29ad6e08ccbc8475525
ShalakaP02 <shala.p02@gmail.com>
2023-03-20 21:25:44 +0530

    practice dsa

---
+++ src/LeetCode/Arrays/PascalsTriangle.java
@@ -0,0 +1,31 @@
+package LeetCode.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        System.out.println(generate(5).toString());
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        List< List<Integer>> arr = new ArrayList<>();
+        ArrayList<Integer> first = new ArrayList<>();
+        first.add(1);
+        arr.add(first);
+
+        for (int i = 1; i < numRows; i++) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            List<Integer> prev = arr.get(i-1);
+            for (int j = 1; j < prev.size(); j++)
+            {
+                temp.add( prev.get(j-1)+prev.get(j) );
+            }
+            temp.add(1);
+            arr.add( temp);
+        }
+        return arr;
+    }
+}
---
+++ src/LeetCode/Arrays/PascalsTriangleII.java
@@ -0,0 +1,32 @@
+package LeetCode.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangleII {
+    public static void main(String[] args) {
+        System.out.println(generate(1).toString());
+    }
+
+    public static List<Integer> generate(int rowIndex) {
+
+
+        List< List<Integer>> arr = new ArrayList<>();
+        List<Integer> opArrayList = new ArrayList<>();
+        opArrayList.add(1);
+        arr.add(opArrayList);
+
+        for (int i = 1; i <= rowIndex; i++) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            List<Integer> prev = arr.get(i-1);
+            for (int j = 1; j < prev.size(); j++)
+            {
+                temp.add( prev.get(j-1)+prev.get(j) );
+            }
+            temp.add(1);
+            arr.add( temp);
+        }
+        return arr.get(rowIndex);
+    }
+}
